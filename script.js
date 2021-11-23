0bea6c2b386e5877ede1097b711a5da29770b498
albinroyson <albinroyson5@gmail.com>
2021-11-23 17:56:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var num1=prompt("enter frist number")
+var  num2 = prompt("enter 2nd number");
+
+function add(num1,num2){
+    return(parseInt(num1)+parseInt(num2))
+
+    }
+    alert(add(num1,num2))
+
+
